2eaf40b165988328fa4da8dc9f78ba3527bf4a5f
alberto360 <alberto@edge360.com>
2015-01-13 18:55:17 -0500

    simple commit

--- public/js/controllers/guestController.js
+++ public/js/controllers/guestController.js
@@ -16,9 +16,9 @@
 	    };
 
 	    // Disable weekend selection
-	    $scope.disabled = function(date, mode) {
-	        return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
-	    };
+	    // $scope.disabled = function(date, mode) {
+	    //     return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+	    // };
 
 	    $scope.toggleMin = function() {
 	        $scope.minDate = $scope.minDate ? null : new Date();
